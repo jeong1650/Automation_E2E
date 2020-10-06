6bd1d3cc3c70ab0e8c5093e73b5945b697050eb5
9000682 <9000682@partner.onestorecorp.com>
2020-10-06 11:12:31 +0900

    OSC 7.0.0 대응

---
+++ download/v7000/Download_SKT_E2E.java
@@ -0,0 +1,114 @@
+package osc_e2e.download.v7000;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class Download_SKT_E2E extends BaseDriver_Download_SKT {
+
+	/* -----------------------------------------------------------------------------
+	 * @ Editor = Park Cheolmin
+	 * @ TCID = Download_E2E
+	 * -----------------------------------------------------------------------------
+	 */	
+	@Test
+	public void Download_Daily_SKT() throws Exception {
+		TCID = "Download_E2E_SKT ";
+		result = "FAIL";
+		size = driver.manage().window().getSize();
+
+		double totalThreadSleepTime = 0;
+		double totalThreadSleepTimesearch = 0;
+		double totalThreadSleepTimedownload = 0;
+
+		popupCheck();
+
+		Thread.sleep(2000);
+		driver.findElementById("com.skt.skaf.A000Z00040:id/btn_search").click(); // 검색 버튼 선택
+		System.out.println("------searchButtonClick-------");
+
+		Thread.sleep(2000);
+		driver.findElementById("com.skt.skaf.A000Z00040:id/searchbar_keyword_edittext").click(); // 검색어 입력
+		driver.findElementById("com.skt.skaf.A000Z00040:id/searchbar_keyword_edittext").sendKeys("알약");
+		System.out.println("------searchKeywordwrite-------");
+
+		Thread.sleep(2000);
+		driver.findElementById("com.skt.skaf.A000Z00040:id/searchbar_search_icon").click(); // 검색어 입력 후 버튼 선택
+		System.out.println("------searchiconClick-------");
+
+		double startTimesearchEnter = System.nanoTime();
+		System.out.println(startTimesearchEnter);
+
+		Thread.sleep(3000);
+		totalThreadSleepTimesearch += 3;
+		driver.findElementByXPath("//*[@text='알약M - 파일 청소 및 메신저 파일 정리, 국내 1위 무료 바이러스+스미싱 보안']").isDisplayed();
+
+		double endTimesearchEnter = System.nanoTime();
+		System.out.println(endTimesearchEnter);
+		double searchEnterResult = Double.parseDouble(String.format("%.2f",
+				(((endTimesearchEnter - startTimesearchEnter) / 1000000000) - totalThreadSleepTimesearch)));
+
+		Thread.sleep(3000);
+		driver.findElementByXPath("//*[@text='알약M - 파일 청소 및 메신저 파일 정리, 국내 1위 무료 바이러스+스미싱 보안']").click(); // 상품명 선택
+		System.out.println("------goodsDetailenter-------");
+
+		double startTimeCompletein = System.nanoTime();
+		System.out.println(startTimeCompletein);
+
+		Thread.sleep(3000);
+		totalThreadSleepTime += 3;
+		driver.findElementByXPath("//*[@text='알약M - 파일 청소 및 메신저 파일 정리, 국내 1위 무료 바이러스+스미싱 보안']").isDisplayed(); // 상품명 확인
+		System.out.println("------goodsDetailenterComplete-------");
+
+		double endTimeCompletein = System.nanoTime();
+		System.out.println(endTimeCompletein);
+		double CompleteEnterResult = Double.parseDouble(String.format("%.2f",
+				(((endTimeCompletein - startTimeCompletein) / 1000000000) - totalThreadSleepTime)));
+
+		Thread.sleep(3000);
+		driver.findElementById("com.skt.skaf.A000Z00040:id/text").click(); // 다운로드 선택
+
+		double startTimeCompletedownload = System.nanoTime();
+		System.out.println(startTimeCompletedownload);
+
+		Thread.sleep(50000);
+		totalThreadSleepTime += 50;
+		driver.findElementByXPath("//*[@text='실행']").isDisplayed(); // 다운로드 => 실행 버튼으로 바뀜 확인
+
+		double endTimeCompletedownload = System.nanoTime();
+		System.out.println(endTimeCompletedownload);
+		double CompletedownloadResult = Double.parseDouble(String.format("%.2f",
+				(((endTimeCompletedownload - startTimeCompletedownload) / 1000000000) - totalThreadSleepTimedownload)));
+
+		System.out.println("--------------------------------------------------");
+		System.out.println(" / " + " searchEnter E2E Result / " + searchEnterResult + "s");
+		System.out.println(" / " + " CompleteEnter E2E Result / " + CompleteEnterResult + "s");
+		System.out.println(" / " + " Completedownload E2E Result / " + CompletedownloadResult + "s");
+		System.out.println("--------------------------------------------------");
+
+		ResultPrint();
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY/MM/dd kk:mm");
+		Date date = new Date();
+		String time = sdf.format(date);
+
+		StringBuffer csv = new StringBuffer();
+		csv.append(time + "," + searchEnterResult + "," + CompleteEnterResult + "," + CompletedownloadResult + "\n");
+
+		try {
+			PrintWriter pw = new PrintWriter(new FileWriter("C:\\Download_SKT_CSV\\SKT_DL_E2E.csv", true));
+
+			pw.write(csv.toString());
+
+			pw.flush();
+			pw.close();
+		} catch (Exception e) {
+
+		}
+		System.out.println("close");
+
+	}
+
+}
---
+++ payment/v7000/BaseDriver_iAPv17_SKT.java
@@ -0,0 +1,124 @@
+package osc_e2e.payment.v7000;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Before;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+
+public class BaseDriver_iAPv17_SKT {
+
+public static AndroidDriver<AndroidElement> driver;
+public static String result = "";
+//public static double inappEnterResult;
+public static String TCID = "";
+protected Dimension size;
+String productname;
+	
+	@Before
+	public void setUp() throws Exception {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("deviceName", "appium");
+		capabilities.setCapability("unicodeKeyboard", true);
+		capabilities.setCapability("udid", "R3CM406ZMP");
+		capabilities.setCapability("appPackage", "com.onestore.iap.apisample.test1");
+		capabilities.setCapability("appActivity", "com.onestore.iap.apisample.ApiCallActivity");
+		capabilities.setCapability("platformVersion", "10");
+		driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}	
+
+	public void Password() throws Exception {
+	   Thread.sleep(2000);
+	   driver.findElementByXPath("//android.widget.Button[@text='1']").click();
+		   
+	   Thread.sleep(2000);
+	   driver.findElementByXPath("//android.widget.Button[@text='0']").click();
+		   
+	   Thread.sleep(2000);
+	   driver.findElementByXPath("//android.widget.Button[@text='2']").click();
+		   
+	   Thread.sleep(2000);
+	   driver.findElementByXPath("//android.widget.Button[@text='9']").click();
+		   
+	   Thread.sleep(2000);
+	   driver.findElementByXPath("//android.widget.Button[@text='3']").click();
+		   
+	   Thread.sleep(2000);
+	   driver.findElementByXPath("//android.widget.Button[@text='8']").click();
+	}
+
+	public void ResultPrint() throws Exception {
+	   System.out.println("-----------------------------------");
+	   result = "PASS";
+	   System.out.println(" --- " + TCID + result + " ---");   
+//	   System.out.println("IAPv17 inappEnterTime E2E Result / " + inappEnterResult);
+	   System.out.println("-----------------------------------");
+	   driver.quit();	
+	}
+
+	// 좌표, 하단 스크롤
+	public static void upSwipe(double starty) {
+		driver.context("NATIVE_APP");
+		Dimension size = driver.manage().window().getSize();
+		int startY = (int) (size.height * starty);
+		int endY = (int) (size.height * 0.10);
+		int end = size.width / 2;
+		driver.swipe(end, startY, end, endY, 1000);
+	}	
+	
+	@After
+	public void end() {
+		if (result != "PASS") {
+			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			SimpleDateFormat sdf = new SimpleDateFormat("YYYY_MM_dd_kk_mm");
+			Date date = new Date();
+			String FileName = sdf.format(date);
+			try { 
+				FileUtils.copyFile(scrFile, new File("C:\\Program Files (x86)\\Jenkins\\workspace\\IAPv17_SKT_E2E\\"+ FileName +".png"));				
+				File pngOriginal = new File("C:\\Program Files (x86)\\Jenkins\\workspace\\IAPv17_SKT_E2E\\"+ FileName +".png");
+				File pngResized = new File("C:\\Program Files (x86)\\Jenkins\\workspace\\IAPv17_SKT_E2E\\"+ FileName +".png");
+				resizeImage(pngOriginal, pngResized, 360, 777, "png");
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}	
+		}
+		System.out.println("======================================");
+		System.out.println("        IAPv17_SKTResult : " + result);
+//		System.out.println("IAPv17 inappEnterTime E2E Result / " + inappEnterResult);
+		System.out.println("                 END                 ");
+		System.out.println("======================================");
+		driver.quit();		
+	}
+
+	private void resizeImage(File originalImage, File resizedImage, int width, int height, String format) {
+		try {
+			BufferedImage original = ImageIO.read(originalImage);
+			BufferedImage resized = new BufferedImage(width, height, original.getType());
+			Graphics2D g2 = resized.createGraphics();
+			g2.drawImage(original, 0, 0, width, height, null);
+			g2.dispose();
+			ImageIO.write(resized, format, resizedImage);			
+		} catch (Exception e) {
+		}		
+	}
+	
+	
+
+		
+}
