7dd05a2ff6f76c17dee65938265af4d2f08224cb
ONESTORECORP\9000696 <9000696@onestorecorp.com>
2023-09-11 17:31:15 +0900

    날마다 혜택체크 테스트

---
+++ src/test/java/download/v7000/MKT_reword.java
@@ -0,0 +1,42 @@
+package download.v7000;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class MKT_reword extends OSC_Base_text {
+
+	/* -----------------------------------------------------------------------------
+	 * @ Writer = Jeong In-Ho
+	 * @ Date = 2021-11-30
+	 * @ ExpectedResult = 테스트 후 마케팅 사전예약 초기화
+	 * @ Editor =
+	 * @ EditDate =
+	 * @
+	 * -----------------------------------------------------------------------------
+	 */
+
+	@Test
+	public void MKT_CAN() throws Exception {
+		TCID = "날마다 혜택 체크";
+		result = "FAIL";
+		size = driver.manage().window().getSize();
+
+		popupCheck();
+        Thread.sleep(3000);
+
+		driver.findElementById("com.skt.skaf.A000Z00040:id/btn_benefit").click();
+
+		Thread.sleep(3000);
+		driver.findElementByXPath("//android.view.View[@text='날마다 혜택 체크']").click();
+
+		Thread.sleep(3000);
+	
+		downscroll(By.xpath("//android.widget.TextView[@text='오늘의 혜택 받기']")).click();
+
+		Thread.sleep(2000);
+	
+		driver.findElementByXPath("//android.view.View[@text='오늘의 혜택 받기 완료']").isDisplayed();
+		
+		result = "PASS";
+	}
+}
