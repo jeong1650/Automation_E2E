5e4b50ad0f31a4c183864909ed0f189cc3bde54e
ONESTORECORP\9000696 <9000696@onestorecorp.com>
2023-09-15 10:58:09 +0900

    xpath 클릭 못할 시 동작 추가

--- src/test/java/download/v7000/MKT_reword.java
+++ src/test/java/download/v7000/MKT_reword.java
@@ -20,17 +20,23 @@ public class MKT_reword extends OSC_Base_text {
 		TCID = "RewordCheck";
 		result = "FAIL";
 		size = driver.manage().window().getSize();
-
 		popupCheck();
         Thread.sleep(3000);
+		try {
+			driver.findElementById("com.skt.skaf.A000Z00040:id/btn_benefit_layout").click();
+			Thread.sleep(3000);
+		}catch (Exception e){
+			driver.findElementById("com.skt.skaf.A000Z00040:id/btn_benefit").click();
+			Thread.sleep(3000);
+		}
 
-		driver.findElementById("com.skt.skaf.A000Z00040:id/btn_benefit").click();
-
-		Thread.sleep(3000);
-		driver.findElementByXPath("//android.view.View[@text='날마다 혜택 체크']").click();
-
-		Thread.sleep(3000);
-	
+		try {
+			driver.findElementByXPath("//android.view.View[@text='날마다 혜택 체크']").click();
+			Thread.sleep(3000);
+		}catch (Exception e){
+			driver.findElementByXPath("//*[@resource-id='lottie_1']").click();
+			Thread.sleep(3000);
+		}
 		downscroll(By.xpath("//android.widget.TextView[@text='오늘의 혜택 받기']")).click();
 
 		Thread.sleep(2000);
