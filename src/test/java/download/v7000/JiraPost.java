9a92426114a383cf068dbe5e5d80142e62a274f0
ONESTORECORP\9000696 <9000696@onestorecorp.com>
2023-11-08 15:24:58 +0900

    test 어노테이션으로 변경

--- src/test/java/download/v7000/JiraPost.java
+++ src/test/java/download/v7000/JiraPost.java
@@ -3,6 +3,7 @@ package download.v7000;
 
 
 import org.json.simple.JSONObject;
+import org.junit.Test;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -20,118 +21,118 @@ import static download.v7000.Download_SKT_E2E_Test.passcount;
 public class JiraPost extends Download_SKT_E2E_Test {
 //    public static ArrayList<Integer> passlist = new ArrayList<>();
 static double passsum = 0;
-
-    public static void main(String[] args) throws Exception {
-
-        String str = Files.readString(Paths.get("C:\\Download_SKT_Result\\SKT_DL_result.txt"));
-        double total_Scripts = 26;
-        double failcount;
-
-
-        failcount = total_Scripts - PcountSum;
-
-        double Total_Percent = PcountSum / total_Scripts;
-
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = new Date(System.currentTimeMillis());
-
-        JSONObject Bon = new JSONObject();
-        JSONObject data1 = new JSONObject();
-        data1.put("key","QARESULT");
-        JSONObject data2 = new JSONObject();
-        data2.put("id","11341");
-
-        Bon.put("project",data1);
-        Bon.put("summary",TCID);
-        Bon.put("description",str);
-        Bon.put("issuetype",data2);
-        Bon.put("customfield_12145",PcountSum);
-        Bon.put("customfield_12146",failcount);
-        Bon.put("customfield_12147",total_Scripts);
-        Bon.put("customfield_12148",Math.round(Total_Percent*100));
-        Bon.put("duedate",format.format(date));
-
-        JSONObject result = new JSONObject();
-
-        result.put("fields",Bon);
-
-        System.out.println(result);
+@Test
+public void JiraTest() throws Exception{
+    String str = Files.readString(Paths.get("C:\\Download_SKT_Result\\SKT_DL_result.txt"));
+    double total_Scripts = 26;
+    double failcount;
 
 
-        String apiUrl = "https://jira.onestorecorp.com/rest/api/2/issue";
+    failcount = total_Scripts - PcountSum;
 
-        String token = "NDE1MTE4Njk0MTg5OgZlK2oolYQoO22CUcWM9fa+GhS4";
+    double Total_Percent = PcountSum / total_Scripts;
 
-        URL url = null;
-        String readLine = null;
-        StringBuilder buffer = null;
-        OutputStream outputStream = null;
-        BufferedReader bufferedReader = null;
-        BufferedWriter bufferedWriter = null;
-        HttpURLConnection urlConnection = null;
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    Date date = new Date(System.currentTimeMillis());
 
-        int connTimeout = 5000;
-        int readTimeout = 3000;
+    JSONObject Bon = new JSONObject();
+    JSONObject data1 = new JSONObject();
+    data1.put("key","QARESULT");
+    JSONObject data2 = new JSONObject();
+    data2.put("id","11341");
+
+    Bon.put("project",data1);
+    Bon.put("summary",TCID);
+    Bon.put("description",str);
+    Bon.put("issuetype",data2);
+    Bon.put("customfield_12145",PcountSum);
+    Bon.put("customfield_12146",failcount);
+    Bon.put("customfield_12147",total_Scripts);
+    Bon.put("customfield_12148",Math.round(Total_Percent*100));
+    Bon.put("duedate",format.format(date));
+
+    JSONObject result = new JSONObject();
+
+    result.put("fields",Bon);
+
+    System.out.println(result);
+
+
+    String apiUrl = "https://jira.onestorecorp.com/rest/api/2/issue";
+
+    String token = "NDE1MTE4Njk0MTg5OgZlK2oolYQoO22CUcWM9fa+GhS4";
+
+    URL url = null;
+    String readLine = null;
+    StringBuilder buffer = null;
+    OutputStream outputStream = null;
+    BufferedReader bufferedReader = null;
+    BufferedWriter bufferedWriter = null;
+    HttpURLConnection urlConnection = null;
+
+    int connTimeout = 5000;
+    int readTimeout = 3000;
 
 
 
+    try
+    {
+        url = new URL(apiUrl);
+
+        urlConnection = (HttpURLConnection)url.openConnection();
+        urlConnection.setRequestMethod("POST");
+        urlConnection.setConnectTimeout(connTimeout);
+        urlConnection.setReadTimeout(readTimeout);
+        urlConnection.setRequestProperty("Authorization", "Bearer " + token);
+        urlConnection.setRequestProperty("Content-Type", "application/json");
+        urlConnection.setDoOutput(true);
+        urlConnection.setInstanceFollowRedirects(true);
+
+        outputStream = urlConnection.getOutputStream();
+
+        bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+        bufferedWriter.write(result.toString());
+        bufferedWriter.flush();
+
+        buffer = new StringBuilder();
+        if(urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK)
+        {
+            bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
+            while((readLine = bufferedReader.readLine()) != null)
+            {
+                buffer.append(readLine).append("\n");
+            }
+        }
+        else
+        {
+            buffer.append("\"code\" : \""+urlConnection.getResponseCode()+"\"");
+            buffer.append(", \"message\" : \""+urlConnection.getResponseMessage()+"\"");
+        }
+    }
+    catch(Exception ex)
+    {
+        ex.printStackTrace();
+    }
+    finally
+    {
         try
         {
-            url = new URL(apiUrl);
-
-            urlConnection = (HttpURLConnection)url.openConnection();
-            urlConnection.setRequestMethod("POST");
-            urlConnection.setConnectTimeout(connTimeout);
-            urlConnection.setReadTimeout(readTimeout);
-            urlConnection.setRequestProperty("Authorization", "Bearer " + token);
-            urlConnection.setRequestProperty("Content-Type", "application/json");
-            urlConnection.setDoOutput(true);
-            urlConnection.setInstanceFollowRedirects(true);
-
-            outputStream = urlConnection.getOutputStream();
-
-            bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
-            bufferedWriter.write(result.toString());
-            bufferedWriter.flush();
-
-            buffer = new StringBuilder();
-            if(urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK)
-            {
-                bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
-                while((readLine = bufferedReader.readLine()) != null)
-                {
-                    buffer.append(readLine).append("\n");
-                }
-            }
-            else
-            {
-                buffer.append("\"code\" : \""+urlConnection.getResponseCode()+"\"");
-                buffer.append(", \"message\" : \""+urlConnection.getResponseMessage()+"\"");
-            }
+            if (bufferedWriter != null) { bufferedWriter.close(); }
+            if (outputStream != null) { outputStream.close(); }
+            if (bufferedReader != null) { bufferedReader.close(); }
         }
         catch(Exception ex)
         {
             ex.printStackTrace();
         }
-        finally
-        {
-            try
-            {
-                if (bufferedWriter != null) { bufferedWriter.close(); }
-                if (outputStream != null) { outputStream.close(); }
-                if (bufferedReader != null) { bufferedReader.close(); }
-            }
-            catch(Exception ex)
-            {
-                ex.printStackTrace();
-            }
-        }
-
-
-        System.out.println("결과 : " + buffer.toString());
-
-        FileDelete();
     }
+
+
+    System.out.println("결과 : " + buffer.toString());
+
+    FileDelete();
+}
+
     public static void FileDelete(){
         String TextFilePath = "C:\\Download_SKT_Result\\SKT_DL_result.txt";
         File DeleteText = new File(TextFilePath);
