2ce058b641d42f7e5a61563df9a874ebacd81eac
ONESTORECORP\9000696 <9000696@onestorecorp.com>
2023-11-08 10:00:03 +0900

    pass count 비교

--- src/test/java/download/v7000/Download_SKT_E2E_Test.java
+++ src/test/java/download/v7000/Download_SKT_E2E_Test.java
@@ -206,7 +206,9 @@ public class Download_SKT_E2E_Test extends BaseDriver_Download_SKT {
 		String word = "PASS";
 		for(i = 0; i < N_list.size(); i++){  // 저장된 Array의 크기만큼 루프
                if (word.contains(N_list.get(i))){
+				   System.out.println(N_list.get(i));
 				   passcount++;
+				   System.out.println(passcount);
 			   }
 
 		}
